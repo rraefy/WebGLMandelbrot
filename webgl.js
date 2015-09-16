098ac14ad857689ad2086c3f93d5d975a0e4765a
Rafael Reyes <rafael.reyes.alvarez@gmail.com>
2015-09-16 23:01:41 +0800

    Added mouse pan and zoom

--- webgl.js
+++ webgl.js
@@ -20,8 +20,8 @@
     gl.useProgram(program);
 
     var a_position = gl.getAttribLocation(program, "a_position");
-    var u_center = gl.getUniformLocation(program, "u_center");
-    var u_span = gl.getUniformLocation(program, "u_span");
+    var u_offset = gl.getUniformLocation(program, "u_offset");
+    var u_scale = gl.getUniformLocation(program, "u_scale");
     var u_iterations = gl.getUniformLocation(program, "u_iterations");
 
     gl.bindBuffer(gl.ARRAY_BUFFER, gl.createBuffer());
@@ -35,18 +35,38 @@
     gl.enableVertexAttribArray(a_position);
     gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 0, 0);
 
-    window.onload = window.onresize = function() {
-        var dpr = window.devicePixelRatio || 1;
+    var dpr = window.devicePixelRatio || 1;
+    var ppu = 400;
+    var center = [0, 0];
+    var iterations = 100;
+    
+    canvas.onmousemove = () => {
+        if (event.buttons & 1) {
+            center[0] -= event.movementX / ppu * dpr;
+            center[1] += event.movementY / ppu * dpr;
+            render();
+        }
+    }
+
+    canvas.onwheel = () => {
+        var clamp = (min, number, max) => Math.max(min, Math.min(number, max));
+        iterations = clamp(2, iterations * Math.pow(2, event.deltaX / -200), 10000);
+        ppu *= Math.pow(2, event.deltaY / -200);
+        render();
+    }
+    
+    window.onload = window.onresize = () => {
         canvas.width = canvas.clientWidth * dpr;
         canvas.height = canvas.clientHeight * dpr;
         gl.viewport(0, 0, canvas.width, canvas.height);
-
-        var center = [-.5, 0];
-        var scale = 1 / 3;
-    
-        gl.uniform2f(u_center, center[0] - .5 / scale, center[1] - .5 / scale);
-        gl.uniform2fv(u_span, [canvas.width * scale, canvas.height * scale]);
-        gl.uniform1i(u_iterations, 100);
-        gl.drawArrays(gl.TRIANGLES, 0, 6);
+        render();
     };
+    
+    function render() {
+        window.cancelAnimationFrame(render.requestId);
+        gl.uniform2f(u_offset, center[0] - canvas.width / 2 / ppu, center[1] - canvas.height / 2 / ppu);
+        gl.uniform1f(u_scale, 1 / ppu);
+        gl.uniform1i(u_iterations, iterations);
+        render.requestId = window.requestAnimationFrame(() => gl.drawArrays(gl.TRIANGLES, 0, 6));
+    }
 })();
